5955446dcd93d5e1d5dbe88fc2f91c5b76708588
Jon James <jon@threering.com>
2014-04-06 10:15:04 -0400

    more progress

--- packages/activity/lib/activity.js
+++ packages/activity/lib/activity.js
@@ -39,7 +39,34 @@ Activity = (function () {
   }
 
   return {
-    name: "jon"
+
+    /*
+     * Takes an options object containing:
+     *
+     * name (Optional - a unique name for this instance)
+     * types (Optional - array of activity type names)
+     *
+     * * * * * * * * * * * * * * * * * * * * */
+    create: function (obj) {
+      var name = null, types = null;
+
+      // Set up name
+      if (obj.name && isString(obj.name)) {
+        name = obj.name;
+      } else if (obj.name && !isString(obj.name)) {
+        throw new Error("Activity name must be provided as a string.");
+      }
+
+      // Set up types
+      if (obj.types && isArray(obj.types)) {
+        types = obj.types;
+      } else if (obj.types && !isArray(obj.types)) {
+        throw new Error("Activity types must be provided as an array.");
+      }
+
+      // Set up templates
+
+    }
   };
 
 }());
